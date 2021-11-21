892e3fad5c9b29ebe0c24d90203abbe8bfdcc3bb
Andreas Maier <a.maier@systems62.com>
2021-11-21 14:38:37 +0100

    initial commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur ex possimus maiores ratione! Quos minima temporibus soluta explicabo veritatis ducimus repudiandae qui deleniti, est non repellat neque facere consequuntur accusamus.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur ex possimus maiores ratione! Quos minima temporibus soluta explicabo veritatis ducimus repudiandae qui deleniti, est non repellat neque facere consequuntur accusamus.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur ex possimus maiores ratione! Quos minima temporibus soluta explicabo veritatis ducimus repudiandae qui deleniti, est non repellat neque facere consequuntur accusamus.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur ex possimus maiores ratione! Quos minima temporibus soluta explicabo veritatis ducimus repudiandae qui deleniti, est non repellat neque facere consequuntur accusamus.</p>
   </div>
 </template>
